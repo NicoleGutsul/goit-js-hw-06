b22aa34b0d3363bd1c4622080edbd8ca4f0cb9ad
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-01-12 00:13:49 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingr = ingredients.map((item, index) => {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = ingredients[index];
+  return itemEl;
+});
+const listEl = document.querySelector("#ingredients");
+listEl.append(...ingr);
