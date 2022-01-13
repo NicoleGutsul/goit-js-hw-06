4b954ce492d5a59e01e67709046190636064f51d
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-01-13 13:39:49 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const colorValue = document.querySelector(".color");
+const btnColor = document.querySelector(".change-color");
+
+btnColor.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = getRandomHexColor();
+});
